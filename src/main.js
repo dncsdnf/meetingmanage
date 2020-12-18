286d40567a4a9e3bd2427e2fb57e76bb9761e13a
dncsdnf <1005139391@qq.com>
2020-12-18 16:54:15 +0800

    first commit

--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import './plugins/element.js'
+import './assets/css/global.css'
+import './assets/fonts/iconfont.css'
+import './assets/css/reset.css'
 
 Vue.config.productionTip = false
 
